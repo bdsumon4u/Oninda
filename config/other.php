212c93c2fc148f9ffc7dafa90389559c99695677
Sumon Ahmed <bdsumon4u@gmail.com>
2023-01-13 17:34:19 +0600

    Added more couriers

--- config/other.php
+++ config/other.php
@@ -4,8 +4,9 @@ return [
     'postal_code' => false,
     'invoice_sku' => false,
     'couriers' => [
+        'Pathao',
+        'SteadFast',
         'Sundarban',
-        'Bandarban',
     ],
     'min_margin' => 1.20,
     'max_margin' => 1.30,
